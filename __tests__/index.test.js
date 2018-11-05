92f31b360ccc3d8b39835ef6b5a1d2b4776c8e80
murakami <w18191263056@yahoo.co.jp>
2018-11-06 07:41:41 +0800

    add init

---
+++ __tests__/index.test.js
@@ -0,0 +1,9 @@
+// Copyright 2018-present murakami. All Rights Reserved.
+
+'use strict';
+
+import { double } from '../src';
+
+it('works with `import`', () => {
+  expect(double(5)).toBe(10);
+});
---
+++ src/index.js
@@ -0,0 +1,5 @@
+// Copyright 2018-present murakami. All Rights Reserved.
+
+export function double(input) {
+  return input * 2;
+}
